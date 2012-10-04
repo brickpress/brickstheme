71afc193874bb28761d753aea414d2db68f7a304
brickpress <support@brickpress.us>
2012-10-04 13:10:57 -0400

    added inline documentation

--- functions/bricks-slider.php
+++ functions/bricks-slider.php
@@ -2,6 +2,31 @@
 /** 
  * Cubricks Slider Template
  *
+ * Uses nivoSlider by Dev7studios.
+ *
+ * Copyright (c) 2010-2012 Dev7studios
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
  * @package Cubricks Theme
  * @subpackage Functions
  */
@@ -60,16 +85,18 @@ function bricks_featured_slider() {
                 
                 </div><!-- .nivoSlider -->
                                             
-                <?php if( $counter_slider++ > 1 ) : ?>
-				<script type="text/javascript">
-				<!--//--><![CDATA[//><!--
-                jQuery(window).load(function() {
-                    jQuery('#slider').nivoSlider({ pauseTime: parseInt(jQuery('#slider_timer').val() * 1200), pauseOnHover: true, effect: '<?php echo $slider_effects; ?>', captionOpacity: 1, controlNav: true, controlNavThumbs:false, controlNavThumbsFromRel:true, boxCols:8, boxRows:4, manualAdvance: false, afterLoad: function(){ 
-					jQuery('.inner-slider').css('visibility', 'visible');
-                    } });
-                });
-				//--><!]]>
-                </script>
+                <?php // No need to initialize slider if there's only one sticky post.
+				 	  // Disables slider and displays featured image linking to the sticky post.
+					if( $counter_slider++ > 1 ) : ?>
+					<script type="text/javascript">
+                    <!--//--><![CDATA[//><!--
+                    jQuery(window).load(function() {
+                        jQuery('#slider').nivoSlider({ pauseTime: parseInt(jQuery('#slider_timer').val() * 1200), pauseOnHover: true, effect: '<?php echo $slider_effects; ?>', captionOpacity: 1, controlNav: true, controlNavThumbs:false, controlNavThumbsFromRel:true, boxCols:8, boxRows:4, manualAdvance: false, afterLoad: function(){ 
+                        jQuery('.inner-slider').css('visibility', 'visible');
+                        } });
+                    });
+                    //--><!]]>
+                    </script>
                 <?php endif; ?>
             
             <?php endif; // End check for published posts. ?>
